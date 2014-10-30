95b2a9b01f2250bf70e9e50f0a31e35ee5b58f77
raymondma1 <raymondxma@gmail.com>
2014-10-29 21:56:41 -0700

    structures functional shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,38 @@
 var makeStack = function() {
+
+  var obj = {
+    count: 0,
+    storage: {}
+  };
+  _.extend(obj,stackMethods);
+  return obj;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function(){
+    return this.count;
+  },
+  push: function(val){
+    this.storage[this.count]=val;
+    this.count ++;
+  },
+  pop: function(){
+    var temp;
+    if(this.count>0){
+      console.log(this.storage,this.count);
+      temp=this.storage[this.count-1];
+      console.log(temp);
+      delete this.storage[this.count-1];
+      //shift first
+      //delete last
+      this.count --;
+      return temp;
+    }
+    return;
+  }
+};
+
 
 
