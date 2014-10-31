b104f630a45b8a43cb2a8ac798d84a26da907ec2
raymondma1 <raymondxma@gmail.com>
2014-10-30 20:34:16 -0700

    up to set done

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,35 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+    if(this.head!==null){
+      this.tail.next = newNode;
+    } else {
+      this.head = newNode;
+    }
+    this.tail=newNode;
   };
 
   list.removeHead = function(){
+    if(this.head!==null) {
+      var temp= this.head.value;
+      this.head = this.head.next;
+      return temp;
+    }
   };
 
   list.contains = function(target){
+    var recurse=function (n) {
+      if(n===null || n===undefined) {
+        return false;
+      } else if (n.value===target) {
+        return true;
+      } else {
+        return recurse(n.next);
+      }
+    }
+   return recurse(this.head);
   };
-
   return list;
 };
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,13 +7,31 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if(this._storage==undefined) {
+    this._storage={};
+  }
+  this._storage[item]=item;
 };
 
 setPrototype.contains = function(item){
+  for(var key in this._storage){
+    if(key===item) {
+      console.log(key,item,"true");
+      return true;
+    }
+  }
+  console.log(false);
+  return false;
 };
 
 setPrototype.remove = function(item){
+  for(var key in this._storage){
+    if(key===item) {
+      delete this._storage[key];
+    }
+  }
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
