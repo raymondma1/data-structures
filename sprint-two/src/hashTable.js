0f76a81b965c4cccddbb96ae11b875441b33b67c
raymondma1 <raymondxma@gmail.com>
2014-10-31 14:35:02 -0700

    hasttable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,18 +5,24 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i]=v;
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var i = getIndexBelowMaxForKey(k, this._limit).toString();
+  for(var key in this._storage) {
+    if(key===i) {
+      return this._storage[i];
+    }
+  }
+  return "Not found";
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i] = null;
 
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,20 +2,37 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 
 
 
-var treeMethods = {};
+var treeMethods= {};
 
 treeMethods.addChild = function(value){
-
+  if(this.children===undefined) {
+    this.children= [];
+  }
+  this.children.push(makeTree(value));
 };
 
-treeMethods.contains = function(target){
-
+treeMethods.contains = function(target, node,res){
+  node=node || this;
+  res = res || false;
+  if(node.value===target) {
+    return true;
+  }
+  else if (node.children!==undefined) {
+    for(var i=0; i<node.children.length;i++) {
+      res=this.contains(target,node.children[i],res);
+      if(res===true){
+        return res;
+      }
+    }
+  }
+  return res;
 };
 
 
